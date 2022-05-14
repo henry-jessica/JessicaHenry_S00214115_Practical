5815b8e40b26e959b93328aa142b12a58df73f94
henry-jessica <jessica.henry@mail.itsligo.ie>
2022-05-14 17:35:51 +0100

    2. Create a class called Car with properties

---
+++ JessicaHenry_S00214115_Practical/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JessicaHenry_S00214115_Practical
+{
+    public class Car
+    {
+        //propreties 
+        public string Name{ get; set; }
+        public string Description{ get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Km { get; set; }
+        public decimal Price { get; set; }
+        public DateTime DateRelease { get; set; }
+        public string Car_Image { get; set; }
+        public string Brand_Icon { get; set; }
+
+        //constructors
+
+        //methods
+     
+
+    }
+}
